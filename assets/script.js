4a1d5f5c6b39dd0f01b95da664d07ab02d3c03c1
bfrazier <bthehippie2@gmail.com>
2022-10-06 18:11:30 -0400

    add javascrpit file; create loop to the console.

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// if (topic === 'HTML') {
+//     console.log("let's study HTML!");
+// } else if (topic === 'CSS') {
+//     console.log("let's study CSS!");
+// } else if (topic === 'Git') {
+//     console.log("let study Git!");
+// } else if (topic === 'JavaScript') {
+//     console.log("let's study JavaScript!");
+// } else {
+//     console.log('Please try again!');
+// }
+
+// var shapes = ["triangle", "square", "pentagon", "circle"];
+// console.log(shapes[0]);
+
+for(var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
