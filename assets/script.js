aae815e0fcdbe3cd1b59cb536637081674777c60
bfrazier <bthehippie2@gmail.com>
2022-10-11 18:01:12 -0400

    write javascript. functions log to console

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,33 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-// if (topic === 'HTML') {
-//     console.log("let's study HTML!");
-// } else if (topic === 'CSS') {
-//     console.log("let's study CSS!");
-// } else if (topic === 'Git') {
-//     console.log("let study Git!");
-// } else if (topic === 'JavaScript') {
-//     console.log("let's study JavaScript!");
-// } else {
-//     console.log('Please try again!');
-// }
+function listTopics(){
+    for (var x = 0; x < topics.length; x++){
+        console.log(topics[x]);
+    }
+   }
+
+function selectTopic(){
+    if (randomTopic === 'HTML') {
+        console.log("let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("let's study CSS!");
+    } else if (randomTopic === 'Git') {
+        console.log("let study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+console.log('here are the topics we learned through prework:');
+listTopics();
+console.log('what should we study first?');
+selectTopic();
 
 // var shapes = ["triangle", "square", "pentagon", "circle"];
 // console.log(shapes[0]);
 
-for(var x = 0; x < topics.length; x++){
-    console.log(topics[x]);
-}
+
+
